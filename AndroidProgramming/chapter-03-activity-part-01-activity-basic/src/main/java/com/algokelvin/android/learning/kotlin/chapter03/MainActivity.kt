dfb965b5bdb54fc7de6d43551c2c01b02526bb91
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-01 14:19:28 +0700

    Set Log Debug and Call onStart and onResume

--- AndroidProgramming/chapter-03-activity-part-01-activity-basic/src/main/java/com/algokelvin/android/learning/kotlin/chapter03/MainActivity.kt
+++ AndroidProgramming/chapter-03-activity-part-01-activity-basic/src/main/java/com/algokelvin/android/learning/kotlin/chapter03/MainActivity.kt
@@ -2,6 +2,7 @@ package com.algokelvin.android.learning.kotlin.chapter03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("chapter-03", "Run onCreate")
 
         btnOK.setOnClickListener {
             tvYourName.text = "Your name is ${edtYourName.text}"
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("chapter-03", "Run onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("chapter-03", "Run onResume")
     }
 }
