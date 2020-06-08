ae8c9bfe54c4441c9312fcc9d948a7b9ec8bcb91
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-08 20:37:44 +0700

    Add Module Android Kotlin Programing Chapter 006: Intent Part 02: Intent with Data

---
+++ AndroidProgramming/chapter-06-intent-part-02-intent-with-data/src/main/java/com/algokelvin/android/learning/kotlin/chapter06/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.algokelvin.android.learning.kotlin.chapter06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
