b875706a2f8e796fdd6048faeab2b5565c59ccd0
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-08 08:50:37 +0700

    Editing Chapter 05

--- AndroidProgramming/chapter-05-intent-part-01-intent-basic/src/main/java/com/algokelvin/android/learning/kotlin/chapter05/MainActivity.kt
+++ AndroidProgramming/chapter-05-intent-part-01-intent-basic/src/main/java/com/algokelvin/android/learning/kotlin/chapter05/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.algokelvin.android.learning.kotlin.chapter05
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnToPageTwo.setOnClickListener {
-            val intentToPageTwo = Intent(this, PageTwoActivity::class.java)
-            startActivity(intentToPageTwo)
-        }
     }
 }
