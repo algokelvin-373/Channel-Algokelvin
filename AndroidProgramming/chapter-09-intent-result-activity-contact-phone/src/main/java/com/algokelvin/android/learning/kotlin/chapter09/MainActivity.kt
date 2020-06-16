30b6f982782b6187072106e745f5b8f0488028ee
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-16 11:55:48 +0700

    Make method just for Contact Phone

--- AndroidProgramming/chapter-09-intent-result-activity-contact-phone/src/main/java/com/algokelvin/android/learning/kotlin/chapter09/MainActivity.kt
+++ AndroidProgramming/chapter-09-intent-result-activity-contact-phone/src/main/java/com/algokelvin/android/learning/kotlin/chapter09/MainActivity.kt
@@ -17,9 +17,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     companion object {
         const val REQUEST_SELECT_PHONE_NUMBER = 1002
-        const val CAMERA_REQUEST = 1888
-        const val PERMISSION_CODE_TAKE_FROM_GALLERY = 1564
-        const val IMAGE_PICK_CODE = 1000
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,25 +30,6 @@ class MainActivity : AppCompatActivity() {
                     getContact()
             }
         }
-
-        btnCamera.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-                    requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST)
-                else
-                    takeImageFromCamera()
-            }
-        }
-
-        btnGallery.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permissions, PERMISSION_CODE_TAKE_FROM_GALLERY)
-                }
-                else takeImageFromGallery()
-            }
-        }
     }
 
     private fun getContact() {
@@ -63,30 +41,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun takeImageFromCamera() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (cameraIntent.resolveActivity(packageManager) != null) startActivityForResult(cameraIntent, CAMERA_REQUEST)
-    }
-
-    private fun takeImageFromGallery() {
-        val intent = Intent(Intent.ACTION_PICK) // take image from gallery
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
-    }
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when(requestCode) {
             REQUEST_SELECT_PHONE_NUMBER -> {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) getContact()
                 else Toast.makeText(this, "Contact Phone Permission denied", Toast.LENGTH_SHORT).show()
-            }
-            CAMERA_REQUEST -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) takeImageFromCamera()
-                else Toast.makeText(this, "Camera Permission denied", Toast.LENGTH_SHORT).show()
-            }
-            PERMISSION_CODE_TAKE_FROM_GALLERY -> {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) takeImageFromGallery()
-                else Toast.makeText(this, "Gallery Permission denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
