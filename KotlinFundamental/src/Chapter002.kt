d2182bec11565d37935f8871bfe5db0726f13bcb
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-27 23:18:18 +0700

    Chapter 002: Type Data

---
+++ KotlinFundamental/src/Chapter002.kt
@@ -0,0 +1,24 @@
+//TODO: Chapter 002: Type Data
+
+fun main() {
+    /*
+    4 Type Data: Number, Boolean, String, and Character
+     */
+
+    //TODO: Number
+    val dataNumber = 373
+
+    //TODO: Boolean
+    val dataBoolean = true
+
+    //TODO: String
+    val dataString = "Algokelvin"
+
+    //TODO: Character
+    val dataCharacter = 'K'
+
+    println(dataNumber)
+    println(dataBoolean)
+    println(dataString)
+    println(dataCharacter)
+}
