005e94ad9caeff47e1aae0f60cb70faa2e5ed380
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-06 10:54:48 +0700

    Add Module Android Kotlin Programming Chapter 014-015: New Project

---
+++ AndroidKotlinNewVersion/Chapter-014-015-RecyclerView-Part02/app/src/main/java/com/algokelvin/android/kotlin/recyclerview/chapter_014_015_recyclerview_part02/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.algokelvin.android.kotlin.recyclerview.chapter_014_015_recyclerview_part02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
