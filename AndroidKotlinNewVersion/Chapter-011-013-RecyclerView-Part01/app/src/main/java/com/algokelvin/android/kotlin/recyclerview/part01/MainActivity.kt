35b8198f4bb2775e206c72292f8f8d13651ab64c
kelvin373-ht <kelvin373.ht@gmail.com>
2020-07-08 00:09:54 +0700

    Android Kotlin Chapter 012: RecycleView Part 2 - CRUD ( Insert and Delete )

--- AndroidKotlinNewVersion/Chapter-011-013-RecyclerView-Part01/app/src/main/java/com/algokelvin/android/kotlin/recyclerview/part01/MainActivity.kt
+++ AndroidKotlinNewVersion/Chapter-011-013-RecyclerView-Part01/app/src/main/java/com/algokelvin/android/kotlin/recyclerview/part01/MainActivity.kt
@@ -10,9 +10,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val data = dataDummy()
+
         rvItemData.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = AdapterData(dataDummy())
+            adapter = AdapterData(data)
+        }
+
+        buttonAddData.setOnClickListener {
+            val index = edtIndexDataItem.text.toString().toInt()
+            val item = ArrayList<DataClass>()
+            item.add(DataClass("PHP"))
+            item.add(DataClass("Go"))
+            item.add(DataClass("Javascript"))
+
+            data.addAll(index, item)
+            rvItemData.adapter?.notifyItemRangeInserted(index, item.size)
+        }
+
+        buttonDeleteData.setOnClickListener {
+            val index = 2
+            data.removeAt(index)
+            rvItemData.adapter?.notifyItemRemoved(index)
+        }
+        buttonDeleteDataMultiple.setOnClickListener {
+            val startIndex = 2
+            val endIndex = 5
+            val rangeIndex = endIndex - startIndex
+            data.subList(startIndex, endIndex).clear()
+            rvItemData.adapter?.notifyItemRangeRemoved(startIndex, rangeIndex)
+        }
+        buttonDeleteAllData.setOnClickListener {
+            data.clear()
+            rvItemData.adapter?.notifyDataSetChanged()
         }
     }
 }
